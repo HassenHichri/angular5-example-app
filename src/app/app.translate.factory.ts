339407b3531e9c26110b468222a5799290a8616a
Hassen Hichri <hassen.hichri@outlook.com>
2017-12-25 15:52:56 +0100

    translate config

--- src/app/app.translate.factory.ts
+++ src/app/app.translate.factory.ts
@@ -2,5 +2,5 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient} from '@angular/common/http';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '/angular5-example-app/assets/i18n', '.json');
+  return new TranslateHttpLoader(http, '/angular5-example-app/assets/i18n/', '.json');
 }
